7d57ef71d22fb0d7d916a3ea3d62abe29872849d
Amit Maurya <97939838+Amit99100@users.noreply.github.com>
2023-11-14 22:14:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+function gotoWeb() {
+
+    const link = document.getElementsByClassName("websitelink");
+    location.href = "https://amit99100.github.io/WeatherApp/";
+
+}
+
+
+function gotoWeb1() {
+
+    const link = document.getElementsByClassName("websitelink");
+    location.href = "https://amit99100.github.io/Password_Generator/";
+}
+function gotoWeb3() {
+
+    const link = document.getElementsByClassName("websitelink");
+    location.href = "https://amit99100.github.io/Interactive-A-Pathfinding-Web-App/";
+}
