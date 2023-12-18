429ea9d60c61efb2be9317eda6621447a2435e95
Amit Maurya <97939838+Amit99100@users.noreply.github.com>
2023-12-18 23:18:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ function gotoWeb1() {
     const link = document.getElementsByClassName("websitelink");
     location.href = "https://amit99100.github.io/Password_Generator/";
 }
+
 function gotoWeb3() {
 
     const link = document.getElementsByClassName("websitelink");
     location.href = "https://amit99100.github.io/Interactive-A-Pathfinding-Web-App/";
+}
+function gotoWeb4() {
+
+    const link = document.getElementsByClassName("websitelink");
+    location.href = "https://namasteconnect.onrender.com/";
 }
